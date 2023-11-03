cfdc6b02672a9faa58c68ae246855bd5bc93f3a2
EricJPogue <epogue@epogue.com>
2023-11-03 13:30:59 -0500

    Update files.

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/test', function(request, response) {
 	response.send('Node.js and Express running on port='+port)
 })
 
+// Load your JSON data
+const favoritePlaces = require('./FavoritePlaces.json');
+
+// Create a route that serves the JSON data
+app.get('/api/favorite-places', (req, res) => {
+  res.json(favoritePlaces);
+});
+
 app.listen(port, function() {
 	console.log("Server is running at http://localhost:3000/")
 })
